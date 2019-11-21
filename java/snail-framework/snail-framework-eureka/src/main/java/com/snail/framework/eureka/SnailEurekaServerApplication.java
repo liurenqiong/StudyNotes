72a1a032b7e46d51b3ae6780151d68c2513a6197
liurenqiong <liurenqiong@bidduo.com>
2019-11-21 16:24:38 +0800

    代码提交

--- java/snail-framework/snail-framework-eureka/src/main/java/com/snail/framework/eureka/SnailEurekaServerApplication.java
+++ java/snail-framework/snail-framework-eureka/src/main/java/com/snail/framework/eureka/SnailEurekaServerApplication.java
@@ -3,6 +3,7 @@ package com.snail.framework.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 /**
  * 服务注册中心
@@ -11,6 +12,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  *
  */
 @EnableEurekaServer
+@EnableWebSecurity
 @SpringBootApplication
 public class SnailEurekaServerApplication {
 	
--- java/snail-framework/snail-framework-eureka/src/main/java/com/snail/framework/eureka/config/WebSecurityConfigurer.java
+++ java/snail-framework/snail-framework-eureka/src/main/java/com/snail/framework/eureka/config/WebSecurityConfigurer.java
@@ -13,41 +13,33 @@ import org.springframework.security.web.firewall.HttpFirewall;
 @EnableWebSecurity
 public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
 
-	@Bean
-	public HttpFirewall httpFirewall() {
-		return new DefaultHttpFirewall();
-	}
-
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-
-		super.configure(web);
-		web.httpFirewall(httpFirewall());
-
-	}
-	
-	@Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();     //关闭跨域
-        http.authorizeRequests()   //认证请求
-                .anyRequest()      //对任何请求
-                .authenticated()   //都需要认证
-                .and()
-                .httpBasic();      //使用Spring Security提供的登录界面
-    }
-	
 //	@Bean
-//	public HttpFirewall allowUrlEncodedSlashHttpFirewall() {
-//		StrictHttpFirewall firewall = new StrictHttpFirewall();
-//		firewall.setAllowUrlEncodedSlash(true);
-//		return firewall;
+//	public HttpFirewall httpFirewall() {
+//		return new DefaultHttpFirewall();
 //	}
 //
 //	@Override
 //	public void configure(WebSecurity web) throws Exception {
+//
 //		super.configure(web);
-//		web.httpFirewall(allowUrlEncodedSlashHttpFirewall());
+//		web.httpFirewall(httpFirewall());
 //
 //	}
+	
+//	@Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.csrf().disable();     //关闭跨域
+//        http.authorizeRequests()   //认证请求
+//                .anyRequest()      //对任何请求
+//                .authenticated()   //都需要认证
+//                .and()
+//                .httpBasic();      //使用Spring Security提供的登录界面
+//    }
+
+	@Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable(); // 关闭csrf
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic(); // 开启认证
+    }
 
 }
