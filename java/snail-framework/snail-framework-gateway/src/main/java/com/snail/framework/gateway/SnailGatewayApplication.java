25ead7052044fb61d8eae4de7f219647b7c7bd9d
liurenqiong <liurenqiong@bidduo.com>
2019-11-21 16:25:59 +0800

    网关提交

---
+++ java/snail-framework/snail-framework-gateway/src/main/java/com/snail/framework/gateway/SnailGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.snail.framework.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author snail
+ * @create 2019/2/26.
+ **/
+@SpringBootApplication
+public class SnailGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnailGatewayApplication.class , args);
+    }
+}
---
+++ java/snail-framework/snail-framework-gateway/src/main/java/com/snail/framework/gateway/filters/GlobalLogFilter.java
@@ -0,0 +1,45 @@
+package com.snail.framework.gateway.filters;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author lixuepeng
+ * @create 2019/3/5.
+ **/
+@Slf4j
+@Component
+public class GlobalLogFilter implements GlobalFilter , Ordered {
+
+    private static final String START_TIME = "startTime";
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String info = String.format("Method:{%s} Host:{%s} Path:{%s} Query:{%s}",
+                exchange.getRequest().getMethod().name(),
+                exchange.getRequest().getURI().getHost(),
+                exchange.getRequest().getURI().getPath(),
+                exchange.getRequest().getQueryParams());
+
+        log.info(info);
+
+        exchange.getAttributes().put(START_TIME, System.currentTimeMillis());
+        return chain.filter(exchange).then( Mono.fromRunnable(() -> {
+            Long startTime = exchange.getAttribute(START_TIME);
+            if (startTime != null) {
+                Long executeTime = (System.currentTimeMillis() - startTime);
+                log.info(exchange.getRequest().getURI().getRawPath() + " : " + executeTime + "ms");
+            }
+        }));
+    }
+
+    @Override
+    public int getOrder() {
+        return Ordered.LOWEST_PRECEDENCE;
+    }
+}
